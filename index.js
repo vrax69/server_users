8872ce24f55e0947053e7e54952756842fc0911e
brian <bllanes@newwavepower.net>
2025-07-11 15:33:54 -0500

    quitamos el sino del puerto

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/api/usuarios', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`API usuarios corriendo en http://localhost:${PORT}`);
 });
